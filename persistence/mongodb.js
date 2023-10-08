805bb3b8b9bf622043eb0518c8b2774544611241
DRIES MERCHIERS <DRIES.MERCHIERS@student.hogent.be>
2023-10-08 13:25:03 +0200

    linting errors fixed

--- persistence/mongodb.js
+++ persistence/mongodb.js
@@ -1,18 +1,26 @@
-import { MongoClient } from "mongodb";
-import Boom from "@hapi/boom";
-import generateFakeData from "./generateFakeData.js";
+import {
+  MongoClient
+} from 'mongodb';
+import Boom from '@hapi/boom';
+import generateFakeData from './generateFakeData.js';
 
 let client;
 
 export async function initialize() {
-  client = new MongoClient(process.env.MONGO_URL, { authSource: 'admin', });
+  client = new MongoClient(process.env.MONGO_URL, {
+    authSource: 'admin'
+  });
   await client.connect();
 
   const animals = client.db().collection('animals');
 
   const count = await animals.countDocuments();
   if (count === 0) {
-    await animals.createIndex({ id: 1 }, { unique: true });
+    await animals.createIndex({
+      id: 1
+    }, {
+      unique: true
+    });
     await animals.insertMany(generateFakeData());
     console.log('Fake data generated');
   }
@@ -27,11 +35,19 @@ export async function close() {
 
 export async function getAnimals() {
   const animals = await client.db().collection('animals').find().toArray() || [];
-  return animals.map(({ id, name }) => ({ id, name }));
+  return animals.map(({
+    id,
+    name
+  }) => ({
+    id,
+    name
+  }));
 }
 
 export async function getAnimal(id) {
-  const animal = await client.db().collection('animals').findOne({ id: id });
+  const animal = await client.db().collection('animals').findOne({
+    id: id
+  });
   if (!animal) {
     throw Boom.notFound('Animal not found');
   }
